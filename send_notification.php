f799015fcc7b287ef4030fc45854ba52eaf42f9c
ManuTenorio <tenoriof@uni-potsdam.de>
2017-04-18 14:04:39 +0200

    support multilanguage push notifications to specific courses

--- send_notification.php
+++ send_notification.php
@@ -26,15 +26,19 @@
 require('../../config.php');
 
 $courseid = required_param('id', PARAM_INT);
-$title = required_param('title', PARAM_TEXT);
-$message = required_param('msg', PARAM_TEXT);
+$title_en = required_param('title_en', PARAM_TEXT);
+$title_de = required_param('title_de', PARAM_TEXT);
+$title_es = required_param('title_es', PARAM_TEXT);
+$message_en = required_param('msg_en', PARAM_TEXT);
+$message_de = required_param('msg_de', PARAM_TEXT);
+$message_es = required_param('msg_es', PARAM_TEXT);
 
 require_login();
 if (isguestuser()) {
     die();
 }
 
-global $COURSE;
+global $COURSE, $PAGE;
 
 $context = context_course::instance($COURSE->id);
 
@@ -43,20 +47,49 @@ if (has_capability('block/pushnotification:sendnotification', $context)){
 	$endpoint = get_config('block_pushnotification', 'URL');
 
 	$operation = 'push';
-	$service = 'reflectup';
+	$service = 'reflectup-';
+    $course = $DB->get_record('course', array('id' => $courseid));
 
-	$url = $endpoint.$operation.'?service='.$service.'&message='.urlencode($message).'&msg='.urlencode($message).'&title='.urlencode($title).'&subscriber=*';
+	$service .= $course->idnumber;
+
+	//give the correct formating to user's input
+	$json_object_str= '{';
+	$json_object_str .= '"en":{';
+	$json_object_str .= '"title":"';
+	$json_object_str .= $title_en;
+	$json_object_str .= '",';
+	$json_object_str .= '"message":"';
+	$json_object_str .= $message_en;
+	$json_object_str .= '"},';
+	$json_object_str .= '"de":{';
+	$json_object_str .= '"title":"';
+	$json_object_str .= $title_de;
+	$json_object_str .= '",';
+	$json_object_str .= '"message":"';
+	$json_object_str .= $message_de;
+	$json_object_str .= '"},';
+	$json_object_str .= '"es":{';
+	$json_object_str .= '"title":"';
+	$json_object_str .= $title_es;
+	$json_object_str .= '",';
+	$json_object_str .= '"message":"';
+	$json_object_str .= $message_es;
+	$json_object_str .= '"}}';
+
+
+	$url = $endpoint.$operation.'?service='.$service.'&message='.urlencode($json_object_str).'&subscriber=*';
 
 	$headers = explode("\n", str_replace("\r", "",get_config('block_pushnotification', 'headers')));
 
 	// new HTTP-Request
+
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
 									CURLOPT_RETURNTRANSFER => 1,
 									CURLOPT_HTTPHEADER => $headers,
 									CURLOPT_URL => $url
 									));
-	$result = curl_exec($curl);
+	//$result = curl_exec($curl);
 	curl_close($curl);
 
 	$courseurl = new moodle_url('/course/view.php', array('id' => $courseid));
